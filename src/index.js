82525eaf92ee354ee5525d8f831275e48b46cf41
Andrew <afellman84@gmail.com>
2022-10-11 10:23:27 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// your code here
+  let form = document.querySelector('create-task-form')
+  form.addEventListener('id-submit', (e) => {
+  e.preventDefault()
+  handleToDo(e.target.new-task-description.value)
+  form.requestFullscreen()
+  })
+})
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  p.textContent = `${todo}`
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#todoContainer').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
