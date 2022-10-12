0df3f537c27fcfa3049e5080c039ee2678f51983
Andrew <afellman84@gmail.com>
2022-10-11 21:15:12 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -16,12 +16,22 @@ function handleToDo(e){
   btn.textContent = 'x'
   p.appendChild(btn)
   console.log(p)
+  let date = document.createElement(date)
+  //date.addEventListener('click', handleDelete)
   document.querySelector('#todoContainer').appendChild(p)
-  let input = document.createElement('input')
-  input.addEventListener('click', placeholder='due date')
-  input.appendChild(input)
+  //document.querySelector('#todoContainer2').appendChild(date)
 }
 
 function handleDelete(e) {
   e.target.parentNode.remove()
+}
+
+function backgroundchange(e) {
+  e.onclick.backgroundchange()
+  if (click) === 'blue'
+    <StyleSheet>background = blue</StyleSheet>
+    if (click) === 'red'
+    <StyleSheet>backgroundchange=red</StyleSheet>
+    if(click) === 'yellow'
+    <StyleSheet>background = yellow</StyleSheet> 
 }
